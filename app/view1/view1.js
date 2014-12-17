0377601dd899e0c626532ae32b562769b3e60e39
gperson <grant.lee,person@gmail.com>
2014-12-17 14:47:18 -0600

    Added angular logic to post note

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,15 +10,19 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.controller('testsCtrl', function($scope, $http) {
-	$http.get('http://localhost:4968/getTestData').then(function(res){
+	$http.get('http://localhost:4968/getTestData').success(function(data, status, headers, config) {
 		var objs = [];
-		for(var i = 0; i < res.data.length; i++){
-			var test = res.data[i];
+		for(var i = 0; i < data.length; i++){
+			var test = data[i];
 			test.start = new Date(Date.parse(test.start));
 			test.end = new Date(Date.parse(test.end));
 			objs[i] = test;
 		}
-		$scope.tests = objs;                
+		$scope.tests = objs;  
+	}).error(function(data, status, headers, config) {
+		var objs = [];
+		objs[0] = {name : "Uh-oh error, reparo server.", error : "Couldn't connect to local server on port 4968."};
+		$scope.tests = objs;
 	});
 
 	$scope.toggleNotes = function(id){
@@ -38,14 +42,24 @@ app.controller('testsCtrl', function($scope, $http) {
 /**
  * Controller for notes section
  */
-app.controller('notesCtrl', function($scope) {
+app.controller('notesCtrl', function($scope,$http) {
 	$scope.init = function(notes) {
 		$scope.notesForm = notes;
 	}
 
-	$scope.addNote = function(){
+	$scope.addNote = function($event){
+		//TODO has to be better way to get id
+		var jsonNote = "{ 'id' : " +$($event.target).parent().parent().parent().prev().attr('id') +", 'note' : "+$scope.noteInput+", 'who' : " + $scope.whoInput+" }";	
 		$scope.notesForm.push({'note' : $scope.noteInput, 'who' : $scope.whoInput});
 		$scope.noteInput = '';
 		$scope.whoInput = '';
+
+		$http.post('http://localhost:4968/addNote', jsonNote).success(function(data, status, headers, config) {
+			console.log("SUCCESS");
+		}).error(function(data, status, headers, config) {
+			console.log("ERROR");
+			//TODO Alert user note wasn't saved
+		});
+
 	}
 });
